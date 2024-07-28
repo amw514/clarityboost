058b9f32eb0eee161a59be9515aaee136fb05306
Ali Munir <alimunirwaris1@gmail.com>
2024-07-29 04:38:14 +0500

    setting up auth with clerk auth and config files related to auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
   )
 }
+
+
 
 export default Home
