4ece831ab5c90b04ed517d82a99cd63293d8580a
Ali Munir <alimunirwaris1@gmail.com>
2024-07-28 23:50:02 +0500

    adding routes, folder structure, removing useless code

---
+++ app/(root)/transformations/[id]/add/[types]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddTransformationTypesPage = () => {
+  return (
+    <div>AddTransformationTypesPage</div>
+  )
+}
+
+export default AddTransformationTypesPage
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateTransformation = () => {
+  return (
+    <div>UpdateTransformation</div>
+  )
+}
+
+export default UpdateTransformation
