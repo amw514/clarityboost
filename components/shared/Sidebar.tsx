5e67d305b1f028614fba68956c49ea1143572cf5
Ali Munir <alimunirwaris1@gmail.com>
2024-07-29 21:04:49 +0500

    finishing up sidebar and its positioning

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,17 +1,18 @@
 "use client";
 
 import { navLinks } from "@/constants";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
+import { Button } from "../ui/button";
 
 const Sidebar = () => {
   const pathname = usePathname();
   return (
     <aside className="sidebar">
-      <div className="flex-col size-full flex gap-4 ">
+      <div className="flex flex-col size-full gap-4 ">
         <Link href="/" className="sidebar-logo">
           <Image
             src="/assets/images/logo-text.svg"
@@ -23,7 +24,7 @@ const Sidebar = () => {
         <nav className="sidebar-nav">
           <SignedIn>
             <ul className="sidebar-nav_elements">
-              {navLinks.map((link) => {
+              {navLinks.slice(0, 6).map((link) => {
                 const isActive = link.route === pathname;
                 return (
                   <li
@@ -48,8 +49,43 @@ const Sidebar = () => {
                 );
               })}
             </ul>
+            <ul className="sidebar-nav_elements">
+              {navLinks.slice(6).map((link) => {
+                const isActive = link.route === pathname;
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    <Link href={link.route} className="sidebar-link">
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+              <li className="curser-pointer gap-2 p-4 flex-center">
+                <UserButton />
+              </li>
+            </ul>
             {/* <UserButton /> */}
           </SignedIn>
+
+          <SignedOut>
+            <Button className="bg-purple-gradient button bg-cover" asChild>
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
         </nav>
       </div>
     </aside>
