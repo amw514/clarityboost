ccf743766a9d3a63b23cd3f93fc5a0275bea9a8e
Ali Munir <alimunirwaris1@gmail.com>
2024-08-01 04:20:58 +0500

    setting up forms with react hook form and zod . implementing categories features for submitting instructions

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,13 +1,30 @@
 import Header from "@/components/shared/Header";
 import TransformationForm from "@/components/shared/TransformationForm";
 import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypesPage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationTypesPage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
   const transformation = transformationTypes[type];
   return (
     <>
-    <Header title={transformation.title} subtitle={transformation.subTitle} />
-    <TransformationForm/>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
     </>
   );
 };
